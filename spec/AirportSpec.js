daa963d37990d8765a185fce8b5bd01b5473e5ff
Alex Lemons <alexlemons1@gmail.com>
2015-09-28 17:08:14 +0100

    aiport land method created

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -3,6 +3,7 @@ describe("Airport", function() {
   var default_capacity = 10;
 
   beforeEach(function() {
+    plane1 = new Plane();
     airport = new Airport();
     console.log(airport);
   });
@@ -14,6 +15,12 @@ describe("Airport", function() {
 
   it("should have an array called hanger to store plane objects", function() {
     expect(airport.hanger).toEqual([]);
+    console.log(airport);
+  });
+
+  it("should be able to instruct a plane to land", function() {
+    airport.land(plane1)
+    expect(airport.hanger).toEqual([plane1]);
     console.log(airport);
   });
 
--- src/Airport.js
+++ src/Airport.js
@@ -2,3 +2,9 @@ function Airport(capacity) {
   this.capacity = capacity || 10;
   this.hanger = [];
 };
+
+
+
+Airport.prototype.land = function(plane) {
+  this.hanger.push(plane);
+};
